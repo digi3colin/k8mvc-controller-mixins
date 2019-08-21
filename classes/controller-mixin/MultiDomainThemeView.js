31157c0962990f1c0989c35a11dca7b0b4c02d9e
Colin Leung <colin@digi3studio.com>
2019-08-21 23:10:33 +0800

    isolate MultiDomainThemeView from ControllerMixinView

--- classes/controller-mixin/MultiDomainThemeView.js
+++ classes/controller-mixin/MultiDomainThemeView.js
@@ -1,12 +1,15 @@
 const K8 = require('k8mvc');
 const View = K8.require('View');
-const ControllerMixinView = K8.require('controller-mixin/View');
+const ControllerMixin = K8.require('ControllerMixin');
 
-class MultiDomainThemeView extends ControllerMixinView{
-  async before(){
+class MultiDomainThemeView extends ControllerMixin{
+  getThemePath(){
     const hostname = this.client.request.hostname.split(':')[0];
-    this.themePath = K8.EXE_PATH.replace('/server', '/sites/')  + hostname + '/themes/default/';
+    return K8.EXE_PATH.replace('/server', '/sites/')  + hostname + '/themes/default/';
+  }
 
+  async before(){
+    this.themePath = this.themePath || this.getThemePath();
     this.client.view = this.getView(this.client.layout || 'layout/default', {domain : this.client.request.hostname});
     this.client.getView = (path, data, viewClass = null) => this.getView(path, data, viewClass);
   }
@@ -25,6 +28,8 @@ class MultiDomainThemeView extends ControllerMixinView{
   }
 
   getView(path, data, viewClass = null){
+    this.themePath = this.themePath || this.getThemePath();
+
     if(viewClass)return new viewClass(path, data, this.themePath);
     return new View.defaultViewClass(path, data, this.themePath);
   }
