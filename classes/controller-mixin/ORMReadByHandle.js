b4ce627f2588c7af2591e0825008387e13729a41
Colin Leung <colin@digi3studio.com>
2020-01-15 16:10:17 +0800

    read by handle support template-suffix

--- classes/controller-mixin/ORMReadByHandle.js
+++ classes/controller-mixin/ORMReadByHandle.js
@@ -2,15 +2,25 @@ const K8 = require('k8mvc');
 const ControllerMixin = K8.require('ControllerMixin');
 
 class ORMReadByHandle extends ControllerMixin{
-  action_read_by_handle(){
+  async action_read_by_handle(){
     if(!this.client.model)return;
 
     const model = this.client.model;
     const handle = this.client.request.params.handle;
 
     const mm = new model(null, {database: this.client.db} );
-    this.client.instance = Object.assign(mm, mm.prepare(`SELECT * from ${model.tableName} WHERE handle = ?`).get(handle));
-    if(!this.client.instance.id)this.client.notFound(`${model.tableName} not found`);
+    const instance = Object.assign(mm, mm.prepare(`SELECT * from ${model.tableName} WHERE handle = ?`).get(handle));
+    if(!instance.id)this.client.notFound(`${model.tableName} handle "${handle}" not found`);
+
+    this.client.instance = instance;
+    console.log(instance.template_suffix);
+
+    const prefix = this.client.templatePrefix || this.client.model.jointTablePrefix;
+    const suffix = instance.template_suffix || '';
+    const template = prefix + (suffix !== '' ? ('.' + suffix) : '');
+
+    this.client.templates.read = 'templates/' + template;
+    this.client.view.data.template = template;
   }
 }
 
